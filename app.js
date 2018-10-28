e161085d4732d707af462ebe75d2b6fbca5f2ea0
Tjpkorhonen <44533453+Tjpkorhonen@users.noreply.github.com>
2018-10-28 20:51:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Pertti peräpuikko\n');
+  res.send('Hello world!\n');
 });
 
 app.listen(PORT, HOST);
